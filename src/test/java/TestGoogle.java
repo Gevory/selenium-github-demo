967dcce868aea5e895ecc02d178597b3e0069e3c
Olga Gevorgizova <olga.gevorgizova@synapse.com>
2017-10-23 16:08:38 +0300

    Initial commit

---
+++ src/test/java/TestGoogle.java
@@ -0,0 +1,19 @@
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+
+public class TestGoogle {
+
+    @Test
+    public void googleTest() {
+        FirefoxDriverManager.getInstance().setup();
+        ChromeOptions options = new ChromeOptions();
+        FirefoxOptions opt = new FirefoxOptions();
+        //opt.
+        WebDriver driver = new FirefoxDriver(opt);
+    }
+}
